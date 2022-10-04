4d5f7bbdf7fb2ff58679ae64d0cfb1fd46c81729
denisaiacob <iacobdenisaalexandra49@gmail.com>
2022-10-04 20:02:47 +0300

    exercitiul 1

--- Laborator 1/ex1.py
+++ Laborator 1/ex1.py
@@ -5,15 +5,17 @@ def cmmdc(a, b):
         return cmmdc(b, a % b)
 
 
-nr = input("Pentru cate numere doresti sa aflii CMMDC?\n")
-nr = (int)(nr)
-a = input("Scrie primul numar\n")
-b = input("Scrie al doilea numar:\n")
-a = (int)(a)
-b = (int)(b)
-a = cmmdc(a, b)
-for index in range(3, nr + 1):
-    b = input("Scrie urmatorul numar\n")
+if __name__ == "__main__":
+    nr = input("Pentru cate numere doresti sa aflii CMMDC?\n")
+    nr = (int)(nr)
+    a = input("Scrie primul numar\n")
+    b = input("Scrie al doilea numar:\n")
+    a = (int)(a)
     b = (int)(b)
     a = cmmdc(a, b)
-print("Cel mai mare divizor comun este: %d" % (a));
+    for index in range(3, nr + 1):
+        b = input("Scrie urmatorul numar\n")
+        b = (int)(b)
+        a = cmmdc(a, b)
+    print("Cel mai mare divizor comun este: %d" % (a));
+
