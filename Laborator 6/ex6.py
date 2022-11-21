ea3ab2209a5ccffa2f729133c1a9d8ac536c4489
denisaiacob <iacobdenisaalexandra49@gmail.com>
2022-11-21 20:07:36 +0200

    Exercitiul 6

---
+++ Laborator 6/ex6.py
@@ -0,0 +1,25 @@
+'''
+Write a function that, for a text given as a parameter, censures words that begin and end with vowels.
+Censorship means replacing characters from odd positions with *.
+'''
+import re
+
+vowels = "aeiou"
+
+
+def censorship(text):
+    words = re.findall("[aeiouAEIOU]\w*[aeiouAEIOU]", text)
+    for w in words:
+        newWord = ''
+        for i in range(0, len(w)):
+            if i % 2 == 1:
+                newWord = newWord + '*'
+            else:
+                newWord = newWord + w[i]
+        text = re.sub(w, newWord, text)
+
+    return text
+
+
+if __name__ == "__main__":
+    print(censorship("abca cda Eaaaei ann"))
