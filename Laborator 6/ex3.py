1f22ded0f2a45e470b51a90284710de2a063da0c
denisaiacob <iacobdenisaalexandra49@gmail.com>
2022-11-24 20:39:01 +0200

    Exercitiul 3 - update

--- Laborator 6/ex3.py
+++ Laborator 6/ex3.py
@@ -1,20 +1,24 @@
 '''
-Write a function that receives as a parameter a string of text characters and a list of regular expressions and returns
-a list of strings that match on at least one regular expression given as a parameter.
+Write a function that receives two parameters: a list of strings and a list of regular expressions. The function will return
+a list of the strings that match on at least one regular expression from the list given as parameter.
 '''
 import re
 
 
 def function(text, regexList):
     stringsList = set()
-    for r in regexList:
-        result = re.findall(r, text)
-        for i in result:
-            stringsList.add(i)
+    for t in text:
+        ok = False
+        for r in regexList:
+            if re.match(r + "$", t):
+                ok = True
+        if ok == True:
+            stringsList.add(t)
     stringsList = list(stringsList)
     return stringsList
 
 
 if __name__ == "__main__":
-    regexList = ["\w+", "\d+", "[0-9]{3}"]
-    print(function("Exemplu 123 ", regexList))
+    regexList = ["\d+", "[0-9]{3}"]
+    text = ["Exemplu", "123", "15aa", "aa15"]
+    print(function(text, regexList))
